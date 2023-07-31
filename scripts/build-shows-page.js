febaa712fdfd29e4c0120164e3848728f59f949f
Bilal Hassen <bilalhassen097@gmail.com>
2023-07-30 20:53:30 -0400

    add html elements using javascript  style shows page

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,118 @@
+const showsArray = [
+    {
+       date: "Mon Sept 06 2021",
+       venue: "Ronald Lane ",
+       location: "San Francisco, CA", 
+    },
+    {
+        date: "Tue Sept 21 2021 ",
+        venue: "Pier 3 East ",
+        location: "San Francisco, CA ", 
+    },
+    {
+        date: "Fri Oct 15 2021 ",
+        venue: "View Lounge",
+        location: "San Francisco, CA", 
+    },
+    {
+        date: "Sat Nov 06 2021",
+        venue: "Hyatt Agency",
+        location: "San Francisco, CA", 
+    },
+    {
+        date: "Fri Nov 26 2021",
+        venue: "Moscow Center",
+        location: "San Francisco, CA", 
+    },
+    {
+        date: "Wed Dec 15 2021 ",
+        venue: "Press Club",
+        location: "San Francisco, CA", 
+    },
+]
+
+function createShowsWrapper(showsData) {
+  let showsContainer = document.querySelector(".shows");
+  // div for shows card
+  let showCard = document.createElement("div");
+  showCard.classList.add("shows__card");
+  showsContainer.appendChild(showCard);
+
+  //div for shows date
+  let showsDate = document.createElement("div");
+  showsDate.classList.add("shows__date");
+  showCard.appendChild(showsDate);
+
+  //para for shows date
+  let showsDatePara = document.createElement("p");
+  showsDatePara.classList.add("shows__date-para");
+  showsDatePara.textContent = "DATE";
+  showsDate.appendChild(showsDatePara);
+
+  //second para for shows date
+  let showsDateParaTwo = document.createElement("p");
+  showsDateParaTwo.classList.add("shows__date-para2");
+  showsDateParaTwo.textContent = showsData.date;
+  showsDate.appendChild(showsDateParaTwo);
+
+
+  // div for venue
+  let showsVenue = document.createElement("div");
+  showsVenue.classList.add("shows__venue-para");
+  showCard.appendChild(showsVenue);
+
+  //para for venue div 
+  let showsVenuePara = document.createElement("p");
+  showsVenuePara.classList.add("shows__venue-para");
+  showsVenuePara.textContent = "VENUE";
+  showsVenue.appendChild(showsVenuePara);
+
+  //second para for shows venue 
+  let showsVenueParaTwo = document.createElement("p");
+  showsVenueParaTwo.classList.add("shows__venue-para2");
+  showsVenueParaTwo.textContent = showsData.venue;
+  showsVenue.appendChild(showsVenueParaTwo);
+
+  // div for shows location
+  let showsLocation = document.createElement("div");
+  showsLocation.classList.add("shows__location");
+  showCard.appendChild(showsLocation);
+
+  //para for shows location 
+  let showsLocationPara = document.createElement("p");
+  showsLocationPara.classList.add("shows__location-para");
+  showsLocationPara.textContent = "LOCATION";
+  showsLocation.appendChild(showsLocationPara);
+
+  //para for shows location 
+  let showsLocationParaTwo = document.createElement("p");
+  showsLocationParaTwo.classList.add("shows__location-para2");
+  showsLocationParaTwo.textContent = showsData.location;
+  showsLocation.appendChild(showsLocationParaTwo);
+
+  // button 
+  let showsButton = document.createElement("button");
+  showsButton.classList.add("shows__button");
+  showsButton.textContent = "BUY TICKETS";
+  showCard.appendChild(showsButton);
+
+return showCard;
+
+}
+
+function presentShows(){
+    let showsContainer = document.querySelector(".shows")
+
+    for(let i = 0; i < showsArray.length; i++) {
+        let showsCard = createShowsWrapper(showsArray[i]);
+        showsContainer.appendChild(showsCard);
+    }
+    
+}
+
+presentShows();
+
+// showsArray.forEach((showsElement) => {
+//         const showCard = createShowsWrapper(showsElement);
+//         showsContainer.appendChild(showCard);
+//     });
---
+++ scripts/index-page.js
@@ -0,0 +1,87 @@
+const commentArray =[
+    {
+        name: "Connor Walton",
+        date: "02/17/2021",
+        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+    },
+    {
+        name: "Emilie Beach",
+        date: "01/09/2021",
+        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+        
+    },
+    {
+        name: "Connor Walton",
+        date: "12/20/2020",
+        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+    },
+]
+
+function createCommentContainer (comment) {
+   let commentCard = document.querySelector(".comment");
+    let commentBox = document.createElement("div");
+    commentBox. classList.add("comment__box");
+    commentCard.appendChild(commentBox);
+
+    //div for avatar
+    let commentPic = document.createElement("div");
+    commentPic.classList.add("comment__pic");
+    commentBox.appendChild(commentPic);
+
+    // comment text div // wrapper
+    let textDiv = document.createElement("div");
+    textDiv.classList.add("comment__text-div");
+    commentBox.appendChild(textDiv);
+
+    //div for name 
+    let commentName = document.createElement("div");
+    commentName.classList.add("comment__name");
+    textDiv.appendChild(commentName);
+
+    //para for div name 
+    let namePara = document.createElement("p");
+    namePara.classList.add("comment__name-txt");
+    namePara.textContent = comment.name;
+    commentName.appendChild(namePara);
+
+    // date div
+    let dateDiv = document.createElement("div");
+    dateDiv.classList.add("comment__date");
+    commentName.appendChild(dateDiv);
+
+    //date para 
+    let datePara = document.createElement("p");
+    datePara.classList.add("comment__date");
+    datePara.textContent = comment.date;
+    dateDiv.appendChild(datePara);
+
+    //text div
+    let textBox = document.createElement("div");
+    textBox.classList.add("comment__txt");
+    textDiv.appendChild(textBox);
+
+    let commenTxt = document.createElement("p");
+    commenTxt.classList.add("comment__body-txt");
+    commenTxt.textContent = comment.comment;
+    textBox.appendChild(commenTxt);
+    
+    return commentBox;
+
+}
+
+
+
+
+
+function displayComment () {
+    const commentCardContainer = document.querySelector(".comment");
+
+    commentCardContainer.innerHTML = "";
+
+
+    for (let i = 0; i < commentArray.length; i++){
+        const comment = createCommentContainer(commentArray[i]);
+        commentCardContainer.appendChild(comment); 
+    }
+}
+displayComment();
